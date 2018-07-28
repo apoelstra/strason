e17a6af759be40712d56d88b908d49e1da4edefc
Andrew Poelstra <apoelstra@wpsoftware.net>
2018-07-28 16:47:34 +0000

    add fuzz tests

---
+++ fuzz/fuzz_targets/parse.rs
@@ -0,0 +1,49 @@
+extern crate strason;
+
+use strason::Json;
+
+fn do_test(data: &[u8]) {
+    if let Ok(json) = Json::from_iter(data.iter().map(|x| Ok(*x))) {
+        let bytes = json.to_bytes();
+        assert_eq!(bytes.is_empty(), data.is_empty());
+    }
+}
+
+#[cfg(feature="honggfuzz")]
+#[macro_use]
+extern crate honggfuzz;
+
+#[cfg(feature="honggfuzz")]
+fn main() {
+    loop {
+        fuzz!(|d| { do_test(d) });
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    fn extend_vec_from_hex(hex: &str, out: &mut Vec<u8>) {
+        let mut b = 0;
+        for (idx, c) in hex.as_bytes().iter().enumerate() {
+            b <<= 4;
+            match *c {
+                b'A'...b'F' => b |= c - b'A' + 10,
+                b'a'...b'f' => b |= c - b'a' + 10,
+                b'0'...b'9' => b |= c - b'0',
+                _ => panic!("Bad hex"),
+            }
+            if (idx & 1) == 1 {
+                out.push(b);
+                b = 0;
+            }
+        }
+    }
+
+    #[test]
+    fn duplicate_crash() {
+        let mut a = Vec::new();
+        extend_vec_from_hex("00000", &mut a);
+        super::do_test(&a);
+    }
+}
+
